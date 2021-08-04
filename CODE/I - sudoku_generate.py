797a77853ca1a2fc903d24cc20a30381413d0f94
IshikaDe-2803 <dmishika2002@gmail.com>
2021-08-04 19:27:42 +0530

    remove fills in sudoku fail

--- CODE/I - sudoku_generate.py
+++ CODE/I - sudoku_generate.py
@@ -1,5 +1,7 @@
 
+
 import random
+from random import randint, shuffle
 import math
 
 empty_grid = [[0 for i in range(9)] for j in range(9)]
@@ -10,6 +12,20 @@ def print_grid(grid):
     for row in grid:
         print(row)
 
+def solve_sudoku(grid):  # grid
+    length = len(grid)
+    for i in range(length):
+        for j in range(length):
+            if grid[i][j] == 0:
+                for k in range(1, length + 1):
+                    if (isValidCell(grid, i, j, k)):
+                        grid[i][j] = k
+                        if solve_sudoku(grid):
+                            return True
+                        grid[i][j] = 0
+                return False
+    
+    return True
 
 def generate_sudoku(grid):  # grid
     # length is the length of each row/column
@@ -36,21 +52,71 @@ def generate_sudoku(grid):  # grid
 
     remove_numbers(grid, 64)
     print_grid(grid)
+    #remove_num(grid)
     # The last true ensures that only one solution is displayed and the recursion stops when the solution is found.
     return True
 
     # NOTE: Am still confused as to when we get the correct solution how does the functions stop the recursion!
 
+# def unique_soln(grid):
+#     c = 0
 
+#     if c == 1:
+#         return True
+#     else:
+#         return False
+
+##########################################
 def remove_numbers(grid, count):
     while (count != 0):
         i = random.randint(0, 8)
         j = random.randint(0, 8)
+        value = grid[i][j]
         if (grid[i][j] != 0):
             count -= 1
             grid[i][j] = 0
-    return grid
-
+        #if(not unique_soln(grid)):
+            grid[i][j] = value
+            remove_numbers(grid, count + 1)
+########################################
+'''
+Mere hisaab se yeh code chalna chahiye, but i dont know how to code a function to count number of solutions
+'''
+#     #1. entire row/column or box should have atleast one element
+#     #2. the sudoku should give us one unique solution (minimum: 17 numbers i.e. 64 spaces)
+#     #If the above two conditions are not satisfied, then what??
+    
+#     return grid
+# def remove_num(grid):
+#     attempts = 5 
+#     counter=1
+#     while attempts>0:
+#     #Select a random cell that is not already empty
+#         row = randint(0,8)
+#         col = randint(0,8)
+#         while grid[row][col]==0:
+#             row = randint(0,8)
+#             col = randint(0,8)
+#     #Remember its cell value in case we need to put it back  
+#         backup = grid[row][col]
+#         grid[row][col]=0
+    
+#     #Take a full copy of the grid
+#         copyGrid = []
+#         for r in range(0,9):
+#             copyGrid.append([])
+#             for c in range(0,9):
+#                 copyGrid[r].append(grid[r][c])
+    
+#     #Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
+#         counter=0      
+#         solve_sudoku(copyGrid)   
+#     #If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
+#         if counter!=1:
+#             grid[row][col]=backup
+#         #We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
+#             attempts -= 1
+#             print_grid(grid)
 
 def isValidCell(grid, row, col, num):
     return (not (present_in_Row(grid, row, num)) and not (present_in_Col(grid, col, num)) and not (present_in_Box(grid, row, col, num)))
