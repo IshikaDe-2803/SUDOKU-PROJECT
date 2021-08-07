b55708923b3234b90e6448933ae05ceb3b22a9de
IshikaDe-2803 <dmishika2002@gmail.com>
2021-08-07 15:08:44 +0530

    code clean-up

--- CODE/generation_complete_grid.py
+++ CODE/generation_complete_grid.py
@@ -49,9 +49,6 @@ def present_in_Box(grid, row, col, num):
                 return True
     return False
 
-
-
-
 def solve_sudoku(grid):  # grid
     length = len(grid)
     for i in range(length):
@@ -65,43 +62,5 @@ def solve_sudoku(grid):  # grid
                         grid[i][j] = 0
                 return False
 
-#INTERNET CODE:
-# def remove(grid):
-#     #Start Removing Numbers one by one
-
-#     #A higher number of attempts will end up removing more numbers from the grid
-#     #Potentially resulting in more difficiult grids to solve!
-#     attempts = 5 
-#     counter=1
-#     while attempts>0:
-#     #Select a random cell that is not already empty
-#         row = randint(0,8)
-#         col = randint(0,8)
-#     while grid[row][col]==0:
-#         row = randint(0,8)
-#         col = randint(0,8)
-#     #Remember its cell value in case we need to put it back  
-#     backup = grid[row][col]
-#     grid[row][col]=0
-    
-#     #Take a full copy of the grid
-#     copyGrid = []
-#     for r in range(0,9):
-#         copyGrid.append([])
-#         for c in range(0,9):
-#             copyGrid[r].append(grid[r][c])
-    
-#     #Count the number of solutions that this grid has (using a backtracking approach implemented in the solveGrid() function)
-#     counter=0      
-#     solve_sudoku(copyGrid)   
-#     #If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
-#     if counter!=1:
-#         grid[row][col]=backup
-#         #We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
-#         attempts -= 1
-#     print(grid)
-    
-   
-(generate_sudoku(empty_grid))
-
+generate_sudoku(empty_grid)
 print_grid(empty_grid)
